93d343c28491f1c093524297bfc8afaf47643324
Samuel Adebayo <samuel.adebayo@engagetech.com>
2022-12-16 02:56:02 +0100

    move graphQL config to graphqs module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,12 @@
-import { ApolloDriverConfig, ApolloDriver } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
-import { GraphQLModule } from '@nestjs/graphql';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RestModule } from './rest/rest.module';
 import { GraphqsModule } from './graphqs/graphqs.module';
 
 @Module({
-  imports: [
-    GraphQLModule.forRoot<ApolloDriverConfig>({
-      driver: ApolloDriver,
-      autoSchemaFile: 'schema.gql',
-      sortSchema: true,
-      // path: '/graphi',
-    }),
-    RestModule,
-    GraphqsModule,
-  ],
-  // controllers: [AppController],
-  // providers: [AppService],
+  imports: [RestModule, GraphqsModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
--- src/graphqs/graphqs.module.ts
+++ src/graphqs/graphqs.module.ts
@@ -1,9 +1,19 @@
+import { ApolloDriverConfig, ApolloDriver } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 import { TeamMembersModule } from './team-members/team-members.module';
 
 @Module({
-  imports: [TeamMembersModule],
-  exports: [TeamMembersModule],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'schema.gql',
+      sortSchema: true,
+      // path: '/graphi',
+    }),
+    TeamMembersModule,
+  ],
+  exports: [],
   controllers: [],
   providers: [],
 })
