7fd4a1ae744a45bf99224691a2beecfba23e2eea
Samuel Adebayo <samuel.adebayo@engagetech.com>
2022-12-23 07:21:11 +0100

    enable cors for client side fetching

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:8300',
+  });
   await app.listen(8100);
 }
 bootstrap();
