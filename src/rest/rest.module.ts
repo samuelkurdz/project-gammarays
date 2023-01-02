986ad18d36b7bd8a53943c4d4d3a330e3df4f2f4
Samuel Adebayo <samuel.adebayo@engagetech.com>
2023-01-02 15:26:20 +0100

    generate basic crud for attendance

--- src/rest/rest.module.ts
+++ src/rest/rest.module.ts
@@ -3,9 +3,16 @@ import { CompanyModule } from './company/company.module';
 import { WorkersModule } from './workers/workers.module';
 import { TeamsModule } from './teams/teams.module';
 import { OccurrencesModule } from './occurrences/occurrences.module';
+import { AttendanceModule } from './attendance/attendance.module';
 
 @Module({
-  imports: [CompanyModule, WorkersModule, TeamsModule, OccurrencesModule],
+  imports: [
+    CompanyModule,
+    WorkersModule,
+    TeamsModule,
+    OccurrencesModule,
+    AttendanceModule,
+  ],
   exports: [],
   controllers: [],
   // providers: [PersonService],
